7c197071053f2e9d63364261c1d7bb979ff1ce7b
Alexander Williams <alexanderw885@gmail.com>
2026-07-20 20:12:30 -0700

    create grid on load

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-const body = document.querySelector("body");
-const txt = document.createElement("p");
-txt.textContent = "it works!";
-body.appendChild(txt);
+const container = document.querySelector("#container");
+
+let numSquares = 16;
+
+
+function init() {
+
+    for(let i = 0; i < numSquares; i++) {
+        const div = document.createElement("div");
+        div.classList.add("row");
+        for(let j = 0; j < numSquares; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            div.appendChild(square);
+            console.log(square.classList)
+        }
+        container.appendChild(div);
+    }
+}
+
+init();
+// console.log(container.childNodes)
